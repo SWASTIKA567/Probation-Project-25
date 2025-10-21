e2400f8ae3866c2c95e3eb8854b40538064b0beb
SWASTIKA567 <swastikasingh1055@gmail.com>
2025-10-21 13:50:00 -0400

    add login screen ui

---
+++ workout_planner/lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:workout_planner/home_screen.dart';
+import 'splash_screen.dart';
+import 'login_screen.dart';
+import 'register_screen.dart';
+import 'welcome_screen.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Todo App',
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      routes: {
+        '/': (_) => const SplashScreen(),
+        '/welcome': (_) => const WelcomeScreen(),
+
+        '/login': (_) => const LoginScreen(),
+        '/register': (_) => const RegisterScreen(),
+        '/home': (_) => const HomeScreen(),
+      },
+    );
+  }
+}
