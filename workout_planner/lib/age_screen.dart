37c9cc3e0ed1a7bbb8fe0a0dd26c6db83fae668d
SWASTIKA567 <swastikasingh1055@gmail.com>
2025-10-25 16:22:55 -0400

    Complete Age Screen UI

--- workout_planner/lib/age_screen.dart
+++ workout_planner/lib/age_screen.dart
@@ -34,7 +34,7 @@ class _AgeScreenState extends State<AgeScreen> {
                 ),
                 const SizedBox(height: 8),
                 const Text(
-                  "To provide you with a better\n   experience by knowing your age",
+                  "    To provide you with a better\n experience by knowing your age",
                   style: TextStyle(color: Colors.grey, fontSize: 16),
                 ),
                 const SizedBox(height: 60),
@@ -42,26 +42,26 @@ class _AgeScreenState extends State<AgeScreen> {
                 // Profile Icon
                 CircleAvatar(
                   radius: 40,
-                  backgroundColor: Colors.white.withOpacity(0.1),
+                  backgroundColor: Colors.white,
                   child: const Icon(
                     Icons.person,
-                    color: Colors.white,
-                    size: 50,
+                    color: Colors.black,
+                    size: 60,
                   ),
                 ),
 
-                const SizedBox(height: 40),
+                const SizedBox(height: 60),
 
                 // Age Selector
                 Stack(
                   alignment: Alignment.center,
                   children: [
                     SizedBox(
-                      height: 100,
+                      height: 350,
                       child: ListWheelScrollView.useDelegate(
                         controller: _scrollController,
-                        itemExtent: 40,
-                        diameterRatio: 1.6,
+                        itemExtent: 60,
+                        diameterRatio: 2,
                         physics: const FixedExtentScrollPhysics(),
                         onSelectedItemChanged: (index) {
                           setState(() {
@@ -79,7 +79,7 @@ class _AgeScreenState extends State<AgeScreen> {
                                   color: isSelected
                                       ? Colors.white
                                       : Colors.grey,
-                                  fontSize: isSelected ? 22 : 18,
+                                  fontSize: isSelected ? 38 : 36,
                                   fontWeight: isSelected
                                       ? FontWeight.bold
                                       : FontWeight.normal,
@@ -93,18 +93,18 @@ class _AgeScreenState extends State<AgeScreen> {
                     ),
                     // lines between selected age
                     Positioned(
-                      top: 80,
+                      top: 140,
                       child: Container(
-                        width: 80,
-                        height: 2,
+                        width: 100,
+                        height: 3,
                         color: Colors.deepOrange,
                       ),
                     ),
                     Positioned(
-                      bottom: 80,
+                      bottom: 140,
                       child: Container(
-                        width: 80,
-                        height: 2,
+                        width: 100,
+                        height: 3,
                         color: Colors.deepOrange,
                       ),
                     ),
@@ -113,60 +113,67 @@ class _AgeScreenState extends State<AgeScreen> {
               ],
             ),
             // Bottom buttons
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 12),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  TextButton.icon(
-                    onPressed: () {
-                      Navigator.pop(context); // skip/back
-                    },
-                    icon: const Icon(
-                      Icons.arrow_back_ios,
-                      color: Colors.purpleAccent,
-                    ),
-                    label: const Text(
-                      "Skip",
-                      style: TextStyle(
-                        color: Colors.purpleAccent,
-                        fontSize: 16,
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 25,
+                  vertical: 12,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    ElevatedButton.icon(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color(0xFF9C7B44),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 25,
+                          vertical: 12,
+                        ),
+                      ),
+                      icon: const Icon(Icons.arrow_back_ios, size: 16),
+                      label: const Text(
+                        "Skip",
+                        style: TextStyle(color: Colors.white),
                       ),
                     ),
-                  ),
-                  ElevatedButton.icon(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: selectedAge == null
-                          ? Colors.grey.shade700
-                          : Colors.purpleAccent,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(30),
+
+                    ElevatedButton.icon(
+                      onPressed: selectedAge != null
+                          ? () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => const AgeScreen(),
+                                ),
+                              );
+                            }
+                          : null, // disabled if gender not selected
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color(0xFF9C7B44),
+                        disabledBackgroundColor: Colors.grey.shade700,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 25,
+                          vertical: 12,
+                        ),
                       ),
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 20,
-                        vertical: 12,
+                      icon: const Icon(Icons.arrow_forward_ios, size: 16),
+                      label: const Text(
+                        "Next",
+                        style: TextStyle(color: Colors.white),
                       ),
                     ),
-                    onPressed: selectedAge == null
-                        ? null
-                        : () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => const WeightScreen(),
-                              ),
-                            );
-                          },
-                    icon: const Icon(
-                      Icons.arrow_forward_ios,
-                      color: Colors.white,
-                    ),
-                    label: const Text(
-                      "Next",
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
           ],
